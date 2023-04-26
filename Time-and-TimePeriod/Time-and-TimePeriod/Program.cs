4bf0398abe50c2417f45f6b0fb763655d6a7f0fe
Justyna Malinowska <106835626+justynamalinowska@users.noreply.github.com>
2023-04-26 22:03:30 +0200

    Add files via upload

---
+++ Time-and-TimePeriod/Time-and-TimePeriod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System;
+
+namespace TimeAndTimePeriod
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Time t = new Time("11:23:54");
+            Console.WriteLine(t.Minutes);
+        }
+    }
+}
---
+++ Time-and-TimePeriod/Time-and-TimePeriod/Time.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace TimeAndTimePeriod
+{
+    public struct Time
+    {
+        public readonly byte Hours;
+        public readonly byte Minutes;
+        public readonly byte Seconds;
+
+        public Time(byte hours, byte minutes, byte seconds)
+        {
+            if (hours > 23 || minutes > 59 || seconds > 59)
+                throw new ArgumentOutOfRangeException();
+
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public Time(byte hours, byte minutes)
+        {
+            if (hours > 23 || minutes > 59)
+                throw new ArgumentOutOfRangeException();
+
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = 00;
+        }
+
+        public Time(byte hours)
+        {
+            if (hours > 23)
+                throw new ArgumentOutOfRangeException();
+
+            Hours = hours;
+            Minutes = 00;
+            Seconds = 00;
+        }
+        public Time()
+        {
+            Hours = 00;
+            Minutes = 00;
+            Seconds = 00;
+        }
+
+        public Time(string s) // s = hh:mm:ss
+        {
+            if (s.Length != 8)
+                throw new Exception("Wrong input try with the pattern: hh:mm:ss");
+
+            string[] array = s.Split(':');
+
+            Hours = Byte.Parse(array[0]);
+            Minutes = Byte.Parse(array[1]);
+            Seconds = Byte.Parse(array[2]);
+        }
+
+    }
+}
